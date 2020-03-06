59be9ca5d26554da21102b737465ad0e0815f988
Charlesluis63 <carlos_luis63@hotmail.com>
2020-03-06 13:59:54 -0500

    Culminacion funcionalidad del modal de creacion de personas naturales

--- ventas/personas_naturales/static/js/confirmacion.js
+++ ventas/personas_naturales/static/js/confirmacion.js
@@ -1,67 +1,81 @@
-let vacios = 0;  
 let vacioList = [];
 function Confirmar(){
-    var vacioList=[]
-
-$("input[type=radio]").each(function() {
-    console.log($(this).attr("checked"));
-    if($(this).attr("checked")!=undefined){
-        console.log("si entra")
-        if($(this).val()=="Primaria" || $(this).val()=="Secundaria"){
-            console.log("entro primaria/secundaria");
-            $("#id_ti_tercernivel").attr("class",$("#id_ti_tercernivel").attr("class")+" ignorar");
-            $("#id_un_tercernivel").attr("class",$("#id_un_tercernivel").attr("class")+" ignorar");
-            $("#id_pais_estudio").attr("class",$("#id_pais_estudio").attr("class")+" ignorar");
-            $("#id_tercer_progreso").attr("class",$("#id_tercer_progreso").attr("class")+" ignorar");
-            $("#id_ti_postgrado").attr("class",$("#id_ti_postgrado").attr("class")+" ignorar");
-            $("#id_un_postgrado").attr("class",$("#id_un_postgrado").attr("class")+" ignorar");
-        }else if($(this).val()=="Tercer Nivel"){
-            console.log("entro tercer nivel");
-            $("#id_ti_postgrado").attr("class",$("#id_ti_postgrado").attr("class")+" ignorar");
-            $("#id_un_postgrado").attr("class",$("#id_un_postgrado").attr("class")+" ignorar");
-            $("#id_institucion").attr("class",$("#id_institucion").attr("class")+" ignorar");
-            $("#id_progreso").attr("class",$("#id_progreso").attr("class")+" ignorar");
-        }else{
-            console.log("entro posgrado")
-            $("#id_ti_tercernivel").attr("class",$("#id_ti_tercernivel").attr("class")+" ignorar");
-            $("#id_un_tercernivel").attr("class",$("#id_un_tercernivel").attr("class")+" ignorar");
-            $("#id_institucion").attr("class",$("#id_institucion").attr("class")+" ignorar");
-            $("#id_progreso").attr("class",$("#id_progreso").attr("class")+" ignorar");
-        }
-    }
     
+$("a#confirmar").click(function (e) { 
+    $("input[type=radio]").each(function() {
+        console.log($(this).attr("checked"));
+        if(document.getElementById($(this).attr("id")).checked){
+            console.log("si entra")
+            console.log($(this).val())
+            if($(this).val()=="Primaria" || $(this).val()=="Secundaria"){
+                console.log("entro primaria/secundaria");
+                $("#id_institucion").attr("class",$("#id_institucion").attr("class").replace(/ignorar/g,""));
+                $("#id_progreso").attr("class",$("#id_progreso").attr("class").replace(/ignorar/g,""));
+
+                $("#id_ti_tercernivel").attr("class",$("#id_ti_tercernivel").attr("class")+" ignorar");
+                $("#id_un_tercernivel").attr("class",$("#id_un_tercernivel").attr("class")+" ignorar");
+                $("#id_pais_estudio").attr("class",$("#id_pais_estudio").attr("class")+" ignorar");
+                $("#id_tercer_progreso").attr("class",$("#id_tercer_progreso").attr("class")+" ignorar");
+                $("#id_ti_postgrado").attr("class",$("#id_ti_postgrado").attr("class")+" ignorar");
+                $("#id_un_postgrado").attr("class",$("#id_un_postgrado").attr("class")+" ignorar");
+            }else if($(this).val()=="Tercer Nivel"){
+                console.log("entro tercer nivel");
+                $("#id_ti_tercernivel").attr("class",$("#id_ti_tercernivel").attr("class").replace(/ignorar/g,""));
+                $("#id_un_tercernivel").attr("class",$("#id_un_tercernivel").attr("class").replace(/ignorar/g,""));
+                $("#id_pais_estudio").attr("class",$("#id_pais_estudio").attr("class").replace(/ignorar/g,""));
+                $("#id_tercer_progreso").attr("class",$("#id_tercer_progreso").attr("class").replace(/ignorar/g,""));
+
+                $("#id_ti_postgrado").attr("class",$("#id_ti_postgrado").attr("class")+" ignorar");
+                $("#id_un_postgrado").attr("class",$("#id_un_postgrado").attr("class")+" ignorar");
+                $("#id_institucion").attr("class",$("#id_institucion").attr("class")+" ignorar");
+                $("#id_progreso").attr("class",$("#id_progreso").attr("class")+" ignorar");
+            }else{
+                console.log("entro posgrado")
+                $("#id_ti_postgrado").attr("class",$("#id_ti_postgrado").attr("class").replace(/ignorar/g,""));
+                $("#id_un_postgrado").attr("class",$("#id_un_postgrado").attr("class").replace(/ignorar/g,""));
+                $("#id_pais_estudio").attr("class",$("#id_pais_estudio").attr("class").replace(/ignorar/g,""));
+                $("#id_tercer_progreso").attr("class",$("#id_tercer_progreso").attr("class").replace(/ignorar/g,""));
+
+                $("#id_ti_tercernivel").attr("class",$("#id_ti_tercernivel").attr("class")+" ignorar");
+                $("#id_un_tercernivel").attr("class",$("#id_un_tercernivel").attr("class")+" ignorar");
+                $("#id_institucion").attr("class",$("#id_institucion").attr("class")+" ignorar");
+                $("#id_progreso").attr("class",$("#id_progreso").attr("class")+" ignorar");
+            }
+        }
+        
+        });
+
+    $("input").each(function() {
+        var clases=$(this).attr("class");
+        
+        if($(this).val()=="" && !clases.includes("ignorar")){
+            var label=$(this).parent().parent().children()[0];
+            vacio =$(label).text();
+            vacioList.push(vacio);
+        }
+        
     });
 
-$("input").each(function() {
-    var clases=$(this).attr("class");
-    
-    if($(this).val()=="" && !clases.includes("ignorar")){
-        var label=$(this).parent().parent().children()[0];
-        vacio =$(label).text();
-        vacioList.push(vacio);
-    }
-    
+    $("select").each(function() {
+        var clases=$(this).attr("class");
+        if($(this).val()=="" && !clases.includes("ignorar")){
+            var label=$(this).parent().parent().children()[0];
+            vacio =$(label).text();
+            vacioList.push(vacio);
+        }
+        
+    });
+
+    $("textarea").each(function() {
+        var clases=$(this).attr("class");
+        if($(this).val()=="" && !clases.includes("ignorar")){
+            var label=$(this).parent().parent().children()[0];
+            vacio =$(label).text();
+            vacioList.push(vacio);
+        }
+        
+
 });
-
-$("select").each(function() {
-    var clases=$(this).attr("class");
-    if($(this).val()=="" && !clases.includes("ignorar")){
-        var label=$(this).parent().parent().children()[0];
-        vacio =$(label).text();
-        vacioList.push(vacio);
-    }
-    
-});
-
-$("textarea").each(function() {
-    var clases=$(this).attr("class");
-    if($(this).val()=="" && !clases.includes("ignorar")){
-        var label=$(this).parent().parent().children()[0];
-        vacio =$(label).text();
-        vacioList.push(vacio);
-    }
-    
-
 
 
 
@@ -115,8 +129,7 @@ $("textarea").each(function() {
         // var select = $("select[id]");
         // console.log(vacios);
     parrafo = $("<p></p>");
-    console.log(vacios);
-    parrafo.html("Hay " + String(vacios) + " campos vacíos");
+    parrafo.html("Hay " + String(vacioList.length) + " campos vacíos");
     $("#modal-confirmacion").html("¿Está seguro que desea guardar los datos?");
     $("#modal-confirmacion").append(parrafo);
     vacioList.forEach(function(item,index){
@@ -126,7 +139,6 @@ $("textarea").each(function() {
 
 
     });
-    vacios=0;
     vacioList = [];
     });
     
@@ -134,7 +146,4 @@ $("textarea").each(function() {
 }
 $(document).ready(function () {
     Confirmar();
-
-    
-
-  });
+});

aac7f3e1858743089cb0237289d653b8fd859919
unknown <luis8e@hotmail.com>
2020-03-09 10:33:56 -0500

    se modifico el diseño del modal para confirmar persona natural para que se muestre en dos columnas

--- ventas/personas_naturales/static/js/confirmacion.js
+++ ventas/personas_naturales/static/js/confirmacion.js
@@ -132,10 +132,32 @@ $("a#confirmar").click(function (e) {
     parrafo.html("Hay " + String(vacioList.length) + " campos vacíos");
     $("#modal-confirmacion").html("¿Está seguro que desea guardar los datos?");
     $("#modal-confirmacion").append(parrafo);
+    var cont=0;
+    var padreactual= undefined;
+    //const padre=$("<div>");
+    //padre.attr("class","row");
     vacioList.forEach(function(item,index){
-        p = $("<p></p>");
-        p.html(item);
-        $("#modal-confirmacion").append(p);
+        if((cont%2)==0){
+            p = $("<p></p>");
+            p.attr("class","col-6 text-justify")
+            p.html(item);
+            //$("#modal-confirmacion").append(p);
+            const padre=$("<div>");
+            padre.attr("class","row");
+            padre.append(p);
+            $("#modal-confirmacion").append(padre);
+            padreactual=padre;
+            cont=cont+1;
+        }else{
+            console.log("momazo")
+            p = $("<p></p>");
+            p.attr("class","col-6 text-justify")
+            p.html(item);
+            //$("#modal-confirmacion").append(p);
+            padreactual.append(p);
+            cont=cont+1;
+        }
+        
 
 
     });

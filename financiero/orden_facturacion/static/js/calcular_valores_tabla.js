86c90107e20ca86b63aea38ecf4db53453da56ae
DarkWolf45000 <luis8e@hotmail.com>
2020-03-17 02:08:08 -0500

    se puso el formato a los campos grises de la tabla participantes

--- financiero/orden_facturacion/static/js/calcular_valores_tabla.js
+++ financiero/orden_facturacion/static/js/calcular_valores_tabla.js
@@ -50,10 +50,21 @@ if ($('#participantes-table tbody tr').length > 0) {
     $("#id_descuento_total").val((s[3] - s[5]).toFixed(2));
     $("#id_descuento_fact").val(roundToTwo(((s[3] - s[5]) / s[3]) * 100));
 
-    $("#subtotal").val("$ "+s[3].toFixed(2));
-    $("#valor_total").val('$ '+s[5].toFixed(2));
-    $("#descuento_total").val('$ '+(s[3] - s[5]).toFixed(2));
+    // $("#subtotal").val("$ "+s[3].toFixed(2));
+    // $("#valor_total").val('$ '+s[5].toFixed(2));
+    // $("#descuento_total").val('$ '+(s[3] - s[5]).toFixed(2));
     $("#descuento_fact").val(roundToTwo(((s[3] - s[5]) / s[3]) * 100)+' %');
+
+    $("#subtotal").val(s[3].toFixed(2));
+    $("#valor_total").val(s[5].toFixed(2));
+    $("#descuento_total").val(+(s[3] - s[5]).toFixed(2));
+    back("#subtotal");
+    transform("#subtotal");
+    back("#valor_total");
+    transform("#valor_total");
+    back("#descuento_total");
+    transform("#descuento_total");
+
 }
 else {
     $('#env-sol').attr('disabled',true);
@@ -63,3 +74,41 @@ else {
     $("#id_descuento_fact").val(0);
 }
 
+function back(input){
+    const val=$(input).val();
+    $(input).attr("type","number");
+    $(input).val(parseFloat(val.replace(/,/g,"")));
+    $(input).val(parseFloat($(input).val()).toFixed(2));
+}
+
+//Funcion para establecer Formato de Dinero
+function transform(input){
+    if($(input).val()!=""){
+      $(input).val(parseFloat($(input).val()).toFixed(2));
+    $(input).attr("type","text");
+    console.log($(input).val())
+    var numeroe=$(input).val().split(".")
+    const long= numeroe[0].length;
+    console.log(long)
+    var newnum="";
+    cont=0;
+    for(var i=(long-1);i>=0;i--){
+          if(cont%3==0 && cont!=0){
+            newnum=numeroe[0].charAt(i)+","+newnum;
+            console.log("coma");
+            console.log(i);
+            console.log(numeroe[0].charAt(i));
+            cont+=1;
+          }else{
+            console.log("no coma");
+            console.log(i);
+            console.log(numeroe[0].charAt(i));
+            newnum=numeroe[0].charAt(i)+newnum;
+            cont+=1;
+          }
+    }
+    console.log(newnum+"."+numeroe[1]);
+    $(input).val("$"+newnum+"."+numeroe[1]);
+    }
+    
+  }
